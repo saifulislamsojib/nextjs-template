d8c810fa0ba095309a6943f60771229a85a6c8f8
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-24 00:11:59 +0600

    fix: build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from "react";
-
 const HomePage = () => {
-  const a = "b";
-  useEffect(() => {
-    console.log(a);
-  }, []);
   return (
     <div>
       <h1>Home Page</h1>
