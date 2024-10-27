c9096904af48f5310ef622340387d681a83ffd67
saifulislamsojib <saifulislam.sojib56@gmail.com>
2024-10-27 12:40:46 +0600

    chore: test updated

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,12 +1,11 @@
 import { render } from "@/lib/test.utils";
-import { screen } from "@testing-library/react";
 import HomePage from "./page";
 
 describe("Page", () => {
   it("renders a heading", () => {
-    render(<HomePage />);
+    const { getByRole } = render(<HomePage />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
+    const heading = getByRole("heading", { level: 1 });
 
     expect(heading).toBeInTheDocument();
   });
